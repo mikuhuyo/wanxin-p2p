43a6573c2025efef0291c5435e377175ddbcf428
yuelimin <yueliminvc@outlook.com>
2021-01-05 16:46:51 +0800

    完成开通存管账户数据准备.

--- wanxinp2p-api/src/main/java/com/wanxin/api/depository/DepositoryAgentAPI.java
+++ wanxinp2p-api/src/main/java/com/wanxin/api/depository/DepositoryAgentAPI.java
@@ -1,9 +1,24 @@
 package com.wanxin.api.depository;
 
+import com.wanxin.api.consumer.model.ConsumerRequest;
+import com.wanxin.api.depository.model.GatewayRequest;
+import com.wanxin.common.domain.RestResponse;
+
 /**
+ * <p>
+ * 银行存管系统代理服务API
+ * </p>
+ *
  * @author yuelimin
  * @version 1.0.0
  * @since 1.8
  */
 public interface DepositoryAgentAPI {
+    /**
+     * 开通存管账户
+     *
+     * @param consumerRequest 开户信息
+     * @return
+     */
+    RestResponse<GatewayRequest> createConsumer(ConsumerRequest consumerRequest);
 }
---
+++ wanxinp2p-depository-agent-service/src/main/java/com/wanxin/depository/controller/DepositoryAgentController.java
@@ -0,0 +1,33 @@
+package com.wanxin.depository.controller;
+
+import com.wanxin.api.consumer.model.ConsumerRequest;
+import com.wanxin.api.depository.DepositoryAgentAPI;
+import com.wanxin.api.depository.model.GatewayRequest;
+import com.wanxin.common.domain.RestResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 存管代理服务
+ * </p>
+ *
+ * @author yuelimin
+ * @version 1.0.0
+ * @since 1.8
+ */
+@Api(value = "存管代理服务", tags = "depository-agent")
+@RestController
+public class DepositoryAgentController implements DepositoryAgentAPI {
+    @Override
+    @PostMapping("/l/consumers")
+    @ApiOperation("生成开户请求数据")
+    @ApiImplicitParam(name = "consumerRequest", value = "开户信息", required = true, dataType = "ConsumerRequest", paramType = "body")
+    public RestResponse<GatewayRequest> createConsumer(@RequestBody ConsumerRequest consumerRequest) {
+        return null;
+    }
+}
