2d47a494086f10f03a3d1ffb18475a2fca54d1d2
yuelimin <yueliminvc@outlook.com>
2021-01-26 15:40:47 +0800

    修复银行存管代理Swagger接口配置问题.

---
+++ wanxinp2p-depository-agent-service/src/main/java/com/wanxin/depository/config/SwaggerConfiguration.java
@@ -0,0 +1,43 @@
+package com.wanxin.depository.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author yuelimin
+ * @version 1.0.0
+ * @since 1.8
+ */
+@Configuration
+@EnableSwagger2
+@ConditionalOnProperty(prefix = "swagger", value = {"enable"}, havingValue = "true")
+public class SwaggerConfiguration {
+    @Bean
+    public Docket buildDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInfo())
+                .select()
+                // 要扫描的API(Controller)基础包
+                .apis(RequestHandlerSelectors.basePackage("com.wanxin"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo buildApiInfo() {
+        Contact contact = new Contact("岳立民", "https://github.com/YueLiMin-say", "yueliminvc@outlook.com");
+        return new ApiInfoBuilder()
+                .title("万信金融P2P平台-银行存管代理API文档")
+                .description("银行存管代理api")
+                .contact(contact)
+                .version("1.0.0").build();
+    }
+}
--- wanxinp2p-depository-agent-service/src/main/java/com/wanxin/depository/config/WebConfig.java
+++ wanxinp2p-depository-agent-service/src/main/java/com/wanxin/depository/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.wanxin.depository.config;
 import com.wanxin.depository.interceptor.DepositoryNotifyVerificationInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
@@ -19,5 +20,27 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(notifyVerificationInterceptor).addPathPatterns("/gateway/**");
+    }
+
+    /**
+     * 添加静态资源文件, 外部可以直接访问地址
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+
+        // 解决swagger无法访问
+        registry
+                .addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        // 解决swagger的js文件无法访问
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry
+                .addResourceHandler("doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
     }
 }
