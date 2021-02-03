97599950a8569f694611b931fa63b11de11a28ee
yuelimin <yueliminvc@outlook.com>
2021-02-03 19:54:54 +0800

    更新数据库脚本.

--- resource/sql/ApolloConfigDB.sql
+++ resource/sql/ApolloConfigDB.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS `ApolloConfigDB`;
+CREATE DATABASE  `ApolloConfigDB` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `ApolloConfigDB`;
+
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
