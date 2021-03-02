cebdcff6ad98a295ff69c1104aaa282a33664ea8
yuelimin <yueliminvc@outlook.com>
2021-03-02 19:22:05 +0800

    修复工具类无法获取当前用户.

--- wanxinp2p-transaction-service/src/main/java/com/wanxin/transaction/common/utils/SecurityUtil.java
+++ wanxinp2p-transaction-service/src/main/java/com/wanxin/transaction/common/utils/SecurityUtil.java
@@ -1,10 +1,13 @@
 package com.wanxin.transaction.common.utils;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wanxin.api.account.model.LoginUser;
+import com.wanxin.common.util.EncryptUtil;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * @author yuelimin
@@ -12,22 +15,27 @@ import javax.servlet.http.HttpServletRequest;
  * @since 1.8
  */
 public class SecurityUtil {
+
     /**
      * 获取当前登录用户
      */
     public static LoginUser getUser() {
         ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-
+        LoginUser loginUser = new LoginUser();
         if (servletRequestAttributes != null) {
             HttpServletRequest request = servletRequestAttributes.getRequest();
 
-            Object jwt = request.getAttribute("jsonToken");
-            if (jwt instanceof LoginUser) {
-                return (LoginUser) jwt;
+            Map jwt = JSONObject.parseObject(EncryptUtil.decodeBase64(request.getHeader("jsonToken")), Map.class);
+            if (jwt.get("mobile").toString() != null && !"".equals(jwt.get("mobile").toString())) {
+                loginUser.setMobile(jwt.get("mobile").toString());
+            }
+
+            if (jwt.get("client_id").toString() != null && !"".equals(jwt.get("client_id").toString())) {
+                loginUser.setClientId(jwt.get("client_id").toString());
             }
         }
 
-        return new LoginUser();
+        return loginUser;
     }
 
 }
