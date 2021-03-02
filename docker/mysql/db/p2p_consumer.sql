3bb32d7ad5a669ba46738ac3f8e4a046d3cbe416
yuelimin <yueliminvc@outlook.com>
2021-03-02 18:46:13 +0800

    更新资源文件.

--- docker/mysql/db/p2p_consumer.sql
+++ docker/mysql/db/p2p_consumer.sql
@@ -50,8 +50,8 @@ DROP TABLE IF EXISTS `consumer_details`;
 CREATE TABLE `consumer_details` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `CONSUMER_ID` bigint(20) NOT NULL COMMENT '用户标识',
-  `ID_CARD_PHOTO` varchar(50) DEFAULT NULL COMMENT '身份证照片面标识',
-  `ID_CARD_EMBLEM` varchar(50) DEFAULT NULL COMMENT '身份证国徽面标识',
+  `ID_CARD_PHOTO` varchar(256) DEFAULT NULL COMMENT '身份证照片面标识',
+  `ID_CARD_EMBLEM` varchar(256) DEFAULT NULL COMMENT '身份证国徽面标识',
   `ADDRESS` varchar(50) DEFAULT NULL COMMENT '住址',
   `ENTERPRISE_MAIL` varchar(20) DEFAULT NULL COMMENT '企业邮箱',
   `CONTACT_RELATION` varchar(10) DEFAULT NULL COMMENT '联系人关系',
