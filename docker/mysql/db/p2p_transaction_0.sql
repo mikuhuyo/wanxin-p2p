4027b6343052f4a915c4cf0c7513c196a290552b
yuelimin <yueliminvc@outlook.com>
2021-03-14 16:13:43 +0800

    更新资源文件.

--- docker/mysql/db/p2p_transaction_0.sql
+++ docker/mysql/db/p2p_transaction_0.sql
@@ -45,6 +45,7 @@ CREATE TABLE `project` (
   `STATUS` tinyint(1) DEFAULT NULL COMMENT '可用状态',
   `IS_ASSIGNMENT` tinyint(4) DEFAULT NULL COMMENT '是否是债权出让标',
   `REQUEST_NO` varchar(50) NULL COMMENT '发标请求流水号',
+  `MODIFY_DATE` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='标的信息表';
 
